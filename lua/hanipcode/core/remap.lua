d2d2d83a67bcc2691f9b7c7d63f3eea4c1e951c2
Muhammad Hanif <hanifeij@yahoo.co.id>
2024-08-16 15:20:28 +0700

    feat: updating harpoon, removing substitute

--- lua/hanipcode/core/remap.lua
+++ lua/hanipcode/core/remap.lua
@@ -26,7 +26,5 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
